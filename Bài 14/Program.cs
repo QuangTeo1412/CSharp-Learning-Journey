9d2e153d037081c6070e1bb99f1a8234b69b1dcb
QuangTeo1412 <2251120237@ut.edu.vn>
2026-09-06 20:49:54 +0700

    PROPERTY

---
+++ Bài 14/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bài_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student myStudent = new Student();
+            myStudent.Name = "Quang";
+            myStudent.Age = -5;
+
+            Console.WriteLine($"Name: {myStudent.Name}");
+            Console.WriteLine($"Age: {myStudent.Age}");
+
+        }
+    }
+    class Student
+    {
+        public string Name { get; set; }
+
+        public double Age
+        {
+            get { return age; }
+            set 
+            {
+                if (value >= 0)
+                {
+                    age = value;
+                }
+            }
+        }
+        private double age;
+    }
+}
+
