dd5c9e3a78d686179ee33e659f4e2ef10fff48e8
QuangTeo1412 <2251120237@ut.edu.vn>
2026-09-06 20:50:47 +0700

    PROPERTY + VALIDATION

---
+++ Bài 15/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bài_15
+{
+    class Student
+    {
+        public string Name { get; set; }
+
+        private int age = 20;
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value >= 0)
+                {
+                    age = value;
+                }
+            }
+        }
+
+        private double score = 8.5;
+        public double Score
+        {
+            get { return score; }
+            set
+            {
+                if (value >= 0 && value <= 10)
+                {
+                    score = value;
+                }
+            }
+        }
+    } 
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Student student = new Student();    
+            student.Name = "Quang";
+            student.Age = -20;
+            student.Score = 15;
+
+            Console.WriteLine("Tên: " + student.Name);
+            Console.WriteLine("Tuổi: " + student.Age);
+            Console.WriteLine("Điểm: " + student.Score);
+        }
+    }
+}
