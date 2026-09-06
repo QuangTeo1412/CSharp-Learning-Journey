40fd63d30022ce97e97119d04592a28b049adb38
QuangTeo1412 <2251120237@ut.edu.vn>
2026-09-06 20:17:52 +0700

    test

--- Bài 1/program.cs
+++ Bài 1/program.cs
@@ -10,6 +10,20 @@ namespace CSharp_Learning_Journey
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8; // Đọc chữ tiếng Việt
+
+            Console.WriteLine("Nhập Họ Và Tên:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Nhập Tuổi:");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            int nextYearAge = age + 1;
+
+            Console.WriteLine("Xin Chào Oni-chan " + name);
+            Console.WriteLine("Tuổi của bạn trong năm sau là: " + nextYearAge);
+
+            Console.ReadLine();
         }
     }
 }
