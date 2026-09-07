422f752d37d826a1ddd9ea29890abbbfbe542150
QuangTeo1412 <2251120237@ut.edu.vn>
2026-09-07 18:00:22 +0700

    Method Overload

--- Exercises/Bài 23/Program.cs
+++ Exercises/Bài 23/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace Bài_23
 {
+    class Calculator 
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Calculator calculator = new Calculator();
+            int sum1 = calculator.Add(5, 3);
+            int sum2 = calculator.Add(5, 3, 2);
+            double sum3 = calculator.Add(5.5, 3.2);
+
+            Console.WriteLine("Sum1: " + sum1);
+            Console.WriteLine("Sum2: " + sum2);
+            Console.WriteLine("Sum3: " + sum3);
         }
     }
 }
