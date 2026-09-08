ee9c8a30baa6303a068bd50509fb95c708f66b3e
QuangTeo1412 <2251120237@ut.edu.vn>
2026-09-08 19:29:22 +0700

    Method Overloading.

--- exercise/Bài 24/Program.cs
+++ exercise/Bài 24/Program.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace Bài_24
 {
+    class Printer
+    { 
+        public void print(string message)
+        {
+            Console.WriteLine("Tin nhắn: " + message);
+        }
+        public void print(string message,int number)
+        {
+            Console.WriteLine("Tin nhắn: " + message + ", Số nguyên: " + number);
+        }
+        public void print(int number)
+        {
+            Console.WriteLine("Số nguyên: " + number);
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Printer printer = new Printer();
+            printer.print("Hello");
+            printer.print("Hello", 5);
+            printer.print(10);
         }
     }
 }
