131c1415f6e0d6cd42da3517d899bbd8f3178fde
QuangTeo1412 <2251120237@ut.edu.vn>
2026-09-12 11:25:24 +0700

    Chứng minh override

---
+++ exercise/03-OOP/BÀi 29/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BÀi_29
+{
+    internal class Program
+    {
+        class Animal
+        {
+            public string Name { get; set; }
+            public Animal(string name)
+            {
+                Name = name;
+            }
+            public virtual void MakeSound()
+            {
+                Console.WriteLine("Động vật kêu");
+            }
+        }
+        class Dog : Animal
+        {
+            public string Breed { get; set; }
+            public Dog(string name, string breed) : base(name)
+            {
+                Breed = breed;
+            }
+            public override void MakeSound()
+            {
+                Console.WriteLine("Gâu Gâu");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Animal animal = new Dog("Tuất", "Golden");
+            animal.MakeSound();
+            Console.WriteLine($"Tên: {animal.Name}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ exercise/03-OOP/BÀi 29/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BÀi 29")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BÀi 29")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2bd6c437-43af-4ce3-8a4f-8fb64584417b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
